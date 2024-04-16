726d1d4d7eb5590564789d784d4664b9c4b9d28f
Athsara Fernando <athsarafernando470@gmail.com>
2024-04-16 14:26:48 +0530

    first

---
+++ W3School/myfirstprgram.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+printf("Hello world\n");
+
+
+int age=21;
+int year;
+year=2003;
+
+printf("%d\n",age);
+int x,y;
+x=y=2;
+printf("%d%d\n",y,x);
+int a=4, b=5;
+printf("%d\n",a);
+return 0;
+}
